94d81efc082422766aa29e42ecb6bfa73e7c23c2
marekjh <marekjh1252@gmail.com>
2023-01-21 09:44:55 -0600

    got n pendulums working

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import pygame
 import numpy as np
-from RK import RK4
+from compute import RK4, sigma
+import sys
 
-
+N = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 SIM_DIMENSION = 600
 BUFFER = 150
 SIZE = SIM_DIMENSION + BUFFER
@@ -12,18 +13,13 @@ BLACK = pygame.Color("black")
 RED = pygame.Color("red")
 BLUE = pygame.Color("blue")
 
-pygame.init()
-pygame.display.set_caption("Double Pendulum Sim")
-clock = pygame.time.Clock()
-
 
 class Sim:
-    def __init__(self, *args):
+    def __init__(self, **kwargs):
         self.screen = pygame.display.set_mode((SIZE, SIZE))
         self.trace = Trace()
-        self.mass1 = Mass(RED, 15)
-        self.mass2 = Mass(RED, 15)
-        self.th1, self.th2, self.th1d, self.th2d, self.g, self.m1, self.m2, self.l1, self.l2 = args
+        self.mass = [Mass(1, RED, 15) for _ in range(N)]
+        self.theta, self.thetad, self.g, self.l = kwargs.values()
         self.time_scale = 1
         self.paused = True
         self.adjust_mode = 0
@@ -37,6 +33,10 @@ class Sim:
         self.trace.screen.fill(BLACK)
 
     def main(self):
+        pygame.init()
+        pygame.display.set_caption("N-Pendulum Sim")
+        clock = pygame.time.Clock()
+
         running = True
         while running:
             for event in pygame.event.get():
@@ -61,47 +61,59 @@ class Sim:
             pygame.display.flip()
 
     def compute_next(self):
-        def F1(th1, th2, th1d, th2d): # th1d
-            return th1d
+        def F(th, thd):
+            return thd
         
-        def F2(th1, th2, th1d, th2d): # th2d
-            return th2d
+        # Used info at https://www.researchgate.net/publication/336868500_Equations_of_Motion_Formulation_of_a_Pendulum_Containing_N-point_Masses and
+        # https://travisdoesmath.github.io/pendulum-explainer/ for equations of motion
+        def G(th, thd):
+            a = []
+            b = []
+            for j in range(N):
+                b_entry = 0 
+                for k in range(N):
+                    m_sum = sum(self.mass[i].m*sigma(j, i) for i in range(k, N))
+                    if j == k:
+                        a.append(sum(self.mass[i].m*self.l[j]**2*sigma(j, i) for i in range(N)))
+                    else:
+                        a.append(m_sum*self.l[j]*self.l[k]*np.cos(th[j] - th[k]))
+                    b_entry -= (self.g*self.l[j]*np.sin(th[j])*self.mass[k].m*sigma(j, k) +
+                               m_sum*self.l[j]*self.l[k]*np.sin(th[j] - th[k])*thd[j]*thd[k] + 
+                               m_sum*self.l[j]*self.l[k]*np.sin(th[k] - th[j])*(thd[j] - thd[k])*thd[k])
+                b.append(b_entry)
 
-        def G1(th1, th2, th1d, th2d): # th1dd
-            return (-self.g*(2*self.m1 + self.m2)*np.sin(th1) - self.m2*self.g*np.sin(th1 - 2*th2) - 2*np.sin(th1 - th2)*self.m2*(th2d**2*self.l2 + 
-                    th1d**2*self.l1*np.cos(th1 - th2))) / (self.l1*(2*self.m1 + self.m2 - self.m2*np.cos(2*(th1 - th2))))
+            a = np.array(a).reshape(N, N)
+            b = np.array(b)
+            return np.linalg.solve(a, b)
 
-        def G2(th1, th2, th1d, th2d): # th2dd
-            return (2*np.sin(th1 - th2)*(th1d**2*self.l1*(self.m1 + self.m2) + self.g*(self.m1 + self.m2)*np.cos(th1) + 
-                th2d**2*self.l2*self.m2*np.cos(th1 - th2))) / (self.l2*(2*self.m1 + self.m2 - self.m2*np.cos(2*(th1 - th2))))
-
-        self.th1prev, self.th2prev, self.th1dprev, self.th2dprev = self.th1, self.th2, self.th1d, self.th2d
-        self.th1, self.th2, self.th1d, self.th2d = RK4(F1, G1, F2, G2, self.th1, self.th1d, self.th2, self.th2d, TIME_STEP)
+        self.thetaprev, self.thetadprev = self.theta, self.thetad
+        self.theta, self.thetad = RK4(F, G, self.theta, self.thetad, TIME_STEP)
         
-    def get_cartesian(self, th1, th2):
-        scale = SIM_DIMENSION / (2*(self.l1 + self.l2))
-        x1 = SIZE/2 + scale * self.l1*np.sin(th1)
-        x2 = x1 + scale * self.l2*np.sin(th2)
-        y1 = SIZE/2 + scale * self.l1*np.cos(th1)
-        y2 = y1 + scale * self.l2*np.cos(th2)
-        return x1, y1, x2, y2
+    def get_cartesian(self, th):
+        scale = SIM_DIMENSION / (2*(np.sum(self.l)))
+        x, y = [scale*self.l[0]*np.sin(th[0]) + SIZE/2], [scale*self.l[0]*np.cos(th[0]) + SIZE/2]
+        for i in range(1, N):
+            x.append(x[i-1] + scale*self.l[i]*np.sin(th[i]))
+            y.append(y[i-1] + scale*self.l[i]*np.cos(th[i]))
+        return np.array(x), np.array(y)
 
     def draw(self):
-        x1, y1, x2, y2 = self.get_cartesian(self.th1, self.th2)
+        x, y = self.get_cartesian(self.theta)
 
         if self.trace.on and self.step > 0:
-            x2prev, y2prev = self.get_cartesian(self.th1prev, self.th2prev)[2:]
+            xprev, yprev = self.get_cartesian(self.thetaprev)
             if not self.adjust_mode:
-                self.trace.update(x2, y2, x2prev, y2prev)
+                self.trace.update(x[-1], y[-1], xprev[-1], yprev[-1])
             self.trace.draw(self.screen)
 
-        pygame.draw.line(self.screen, BLACK, (SIZE/2, SIZE/2), (x1, y1), 5)
-        pygame.draw.line(self.screen, BLACK, (x1, y1), (x2, y2), 5)
-        self.mass1.rect.centerx, self.mass1.rect.centery = x1, y1
-        self.mass2.rect.centerx, self.mass2.rect.centery = x2, y2
-        self.mass1.draw(self.screen)
-        self.mass2.draw(self.screen)
-        
+        pygame.draw.line(self.screen, BLACK, (SIZE/2, SIZE/2), (x[0], y[0]), 5)
+        for i in range(1, N):
+                pygame.draw.line(self.screen, BLACK, (x[i-1], y[i-1]), (x[i], y[i]), 5)
+
+        for i in range(N):
+            self.mass[i].rect.centerx, self.mass[i].rect.centery = x[i], y[i]
+            self.mass[i].draw(self.screen)
+            
 
     def handle_keyboard(self, key):
         if key == pygame.K_SPACE:
@@ -112,30 +124,27 @@ class Sim:
             self.trace.screen.fill(BLACK)
 
     def handle_mouse_click(self, mousepos):
-        for i, mass in enumerate((self.mass1, self.mass2)):
+        for i, mass in enumerate(self.mass):
             if mass.rect.collidepoint(mousepos):
                 self.paused = True
                 self.trace.screen.fill(BLACK)
-                if i == 0: 
-                    self.adjust_mode = 1
-                else:
-                    self.adjust_mode = 2
+                self.adjust_mode = i + 1
     
     def handle_mouse_drag(self, mousepos):
-        mouse_x, mouse_y = mousepos
-        mouse_x -= SIZE/2
-        mouse_y -= SIZE/2
-        if self.adjust_mode == 1:
-            self.th1 = np.arctan2(-mouse_y, mouse_x) + np.pi/2
-        elif self.adjust_mode == 2:
-            x1, y1 = self.get_cartesian(self.th1, self.th2)[:2]
-            x1 -= SIZE/2
-            y1 -= SIZE/2
-            self.th2 = np.arctan2(y1 - mouse_y, mouse_x - x1) + np.pi/2
+        if self.adjust_mode:
+            x, y = self.get_cartesian(self.theta)
+            mouse_x, mouse_y = mousepos
+            x -= SIZE/2
+            y -= SIZE/2
+            mouse_x -= SIZE/2
+            mouse_y -= SIZE/2
+            self.theta[self.adjust_mode-1] = np.arctan2(np.sum(y[:self.adjust_mode-1]) - mouse_y, mouse_x - np.sum(x[:self.adjust_mode-1])) + np.pi/2
+
     
     def handle_mouse_up(self):
         if self.adjust_mode:
-            self.th1d, self.th2d, self.step, self.adjust_mode = 0, 0, 0, 0
+            self.thetad = np.zeros(N)
+            self.step, self.adjust_mode = 0, 0
 
 class Trace:
     def __init__(self):
@@ -150,9 +159,10 @@ class Trace:
         screen.blit(self.screen, (0, 0))
 
 class Mass(pygame.sprite.Sprite):
-    def __init__(self, color, radius):
+    def __init__(self, m, color, radius):
         super().__init__()
 
+        self.m = m
         self.image = pygame.Surface((2*radius, 2*radius))
         self.image.set_colorkey(BLACK)
         pygame.draw.circle(self.image, color, (radius, radius), radius)
@@ -163,4 +173,4 @@ class Mass(pygame.sprite.Sprite):
 
 
 if __name__ == "__main__":
-    Sim(np.pi/2, np.pi/2, 0, 0, 9.81, 1, 1, 1, 1) # Default initial conds/parameters
+    Sim(theta=np.ones(N)*np.pi/2, thetad=np.zeros(N)*np.pi/2, g=9.81, l=np.ones(N)) 
